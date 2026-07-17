9670de461d48a8088e2a4e57db7db46caf3d867f
Nayrelis Rumbos <nayrelisrumbos2@gmail.com>
2026-07-16 22:13:22 -0500

    v0.3_interfacesPaqueteRepository_runnay_16072026

---
+++ src/main/java/com/sena/parqueadero/repository/IVehiculoRepository.java
@@ -0,0 +1,25 @@
+package com.sena.parqueadero.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import com.sena.parqueadero.model.Vehiculo;
+
+import jakarta.persistence.LockModeType;
+import lombok.val;
+
+public interface IVehiculoRepository extends JpaRepository<Vehiculo, Long> {
+
+	Optional<Vehiculo> findByPlaca(String placa);
+	
+	boolean existsByPlaca(String placa);
+	
+	
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT v FROM Vehiculo v WHERE v.placa = : placa")
+	Optional<Vehiculo> findByPlacaForUpdate(String placa);
+	
+}
